b7caf4cb40567671cef4b955179731028f603730
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-10-24 09:15:03 +0100

    get_tasks endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """modules"""
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -16,3 +17,8 @@ def create_task(task: Task):
     """create_task endpoint"""
     tasks.append(task)
     return {"Message": "Task added successfully!"}
+
+@app.get("/tasks", response_model=List[Task])
+def get_tasks():
+    """get_tasks endpoint"""
+    return tasks
